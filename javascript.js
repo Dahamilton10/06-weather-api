3f0095a916753b09b17c5e264e0ced0e125ca581
David Hamilton <dahamilton10@yahoo.com>
2019-11-02 17:19:17 -0400

    got weather info for portsmouth, also have it so I can pull a value from the user using the search function.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+var lat = "";
+var lon = "";
 
 
 
@@ -5,8 +7,24 @@
 $.ajax({
     type: "GET",
     url: "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=Portsmouth,us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716",
+    "q=Portsmouth,us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial",
     success: function (response) {
         console.log(response);
+        lat = response.coord.lat;
+        lon = response.coord.lon;
+        console.log(lat, lon);
+        var city = response.name;
+        var temp = response.main.temp;
+        var humidity = response.main.humidity;
+        var wind = response.wind.speed;
+        $("#current-city").text([city]);
+        $("#current-temp").text("Temperature: " + [temp] + " °F");
+        $("#current-humidity").text("Humidity: " + [humidity] + "%");
+        $("#current-wind-speed").text("Wind speed: " + [wind] + " MPH");
     }
+});
+
+$("#search-button").click(function(){
+    var cityQuery = $(".inputQuery").val();
+    console.log(cityQuery);
 });
