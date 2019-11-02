5de078d82660a19b578c787f35f8cf6f78d449ff
David Hamilton <dahamilton10@yahoo.com>
2019-11-02 17:35:42 -0400

    got the user searched cities to appear in current area

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ var lon = "";
 $.ajax({
     type: "GET",
     url: "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=Portsmouth,us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial",
+        "q=Portsmouth,us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial",
     success: function (response) {
         console.log(response);
         lat = response.coord.lat;
@@ -24,7 +24,24 @@ $.ajax({
     }
 });
 
-$("#search-button").click(function(){
+$("#search-button").click(function () {
     var cityQuery = $(".inputQuery").val();
-    console.log(cityQuery);
+    $.ajax({
+        type: "GET",
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${cityQuery},us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial`,
+        success: function (response) {
+            console.log(response);
+            lat = response.coord.lat;
+            lon = response.coord.lon;
+            console.log(lat, lon);
+            var city = response.name;
+            var temp = response.main.temp;
+            var humidity = response.main.humidity;
+            var wind = response.wind.speed;
+            $("#current-city").text([city]);
+            $("#current-temp").text("Temperature: " + [temp] + " °F");
+            $("#current-humidity").text("Humidity: " + [humidity] + "%");
+            $("#current-wind-speed").text("Wind speed: " + [wind] + " MPH");
+        }
+    });
 });
