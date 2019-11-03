200c008215d3e13bf248c784e48ba054cf6a32e2
David Hamilton <dahamilton10@yahoo.com>
2019-11-03 15:11:31 -0500

    got to the point where I can create a 5 day forecast but prior stuff doesnt get remove, the new forecast just goes to the bottom

--- javascript.js
+++ javascript.js
@@ -1,15 +1,9 @@
-var lat = "";
-var lon = "";
-
-
 $.ajax({
     type: "GET",
-    url: "https://api.openweathermap.org/data/2.5/forecast?q=Portsmouth,us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial&cnt=5",
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=Hooksett,us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial&cnt=37",
     success: function (response) {
         console.log(response);
-        lat = response.city.coord.lat;
-        lon = response.city.coord.lon;
-        console.log(lat, lon);
+        // Creating the current weather
         var city = response.city.name;
         var temp = response.list[0].main.temp;
         var humidity = response.list[0].main.humidity;
@@ -18,14 +12,37 @@ $.ajax({
         $("#current-temp").text("Temperature: " + [temp] + " °F");
         $("#current-humidity").text("Humidity: " + [humidity] + "%");
         $("#current-wind-speed").text("Wind speed: " + [wind] + " MPH");
+        // Creating the 5 day weather forecast
+
+        var card = $("<div class='card'>")
+        card.attr("class", "inline");
+
+        for (var x = 0; x < 4; x++) {
+            var i = x * 8 + 5;
+            var dayDiv = $("<div>");
+            var dayDate = $("<h4>");
+            var dayIconDiv = $("<img>");
+            dayIcon = response.list[i].weather[0].icon;
+            var dayTemp = $("<p>");
+            var dayHumidity = $("<p>");
+            dayDiv.attr("class", "bg-primary text-white col-md-3 border rounded inline");
+            dayDate.text(response.list[i].dt_txt);
+            dayIconDiv.attr("src", `http://openweathermap.org/img/wn/${dayIcon}@2x.png`);
+            dayTemp.text("Temperature: " + [response.list[i].main.temp] + " °F");
+            dayHumidity.text("Humidity: " + [response.list[i].main.humidity] + "%");
+            dayDiv.append(dayDate, dayIconDiv, dayTemp, dayHumidity);
+            card.append(dayDiv);
+        };
+
+        $("#five-day-forecast").append(card);
     }
 });
 
 $("#search-button").click(function () {
-    var cityQuery = $(".inputQuery").val();
+    cityQuery = $(".inputQuery").val();
     $.ajax({
         type: "GET",
-        url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityQuery},us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial&cnt=5`,
+        url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityQuery},us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial&cnt=37`,
         success: function (response) {
             console.log(response);
             // creating the input into an li in the history
@@ -47,19 +64,43 @@ $("#search-button").click(function () {
             $("#current-temp").text("Temperature: " + [temp] + " °F");
             $("#current-humidity").text("Humidity: " + [humidity] + "%");
             $("#current-wind-speed").text("Wind speed: " + [wind] + " MPH");
+            // Creating the 5 day weather forecast
+
+            var card = $("<div class='card'>")
+            card.attr("class", "inline");
+
+            for (var x = 0; x < 4; x++) {
+                var i = x * 8 + 5;
+                var dayDiv = $("<div>");
+                var dayDate = $("<h4>");
+                var dayIconDiv = $("<img>");
+                dayIcon = response.list[i].weather[0].icon;
+                var dayTemp = $("<p>");
+                var dayHumidity = $("<p>");
+                dayDiv.attr("class", "bg-primary text-white col-md-3 border rounded inline");
+                dayDate.text(response.list[i].dt_txt);
+                dayIconDiv.attr("src", `http://openweathermap.org/img/wn/${dayIcon}@2x.png`);
+                dayTemp.text("Temperature: " + [response.list[i].main.temp] + " °F");
+                dayHumidity.text("Humidity: " + [response.list[i].main.humidity] + "%");
+                dayDiv.append(dayDate, dayIconDiv, dayTemp, dayHumidity);
+                card.append(dayDiv);
+            };
+
+            $("#five-day-forecast").append(card);
         }
     });
 });
 
 $(document).on("click", "#history", function () {
     console.log(this);
-    var cityQuery = $(this).data( "city" );
+    cityQuery = $(this).data("city");
     console.log(cityQuery);
     $.ajax({
         type: "GET",
-        url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityQuery},us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial&cnt=5`,
+        url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityQuery},us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial&cnt=37`,
         success: function (response) {
             console.log(response);
+            $(".five-day-forecast").innerHTML = "";
             // pulling info from the responce and putting it on the page
             lat = response.city.coord.lat;
             lon = response.city.coord.lon;
@@ -72,6 +113,31 @@ $(document).on("click", "#history", function () {
             $("#current-temp").text("Temperature: " + [temp] + " °F");
             $("#current-humidity").text("Humidity: " + [humidity] + "%");
             $("#current-wind-speed").text("Wind speed: " + [wind] + " MPH");
+            // Creating the 5 day weather forecast
+
+            var card = $("<div class='card'>")
+            card.attr("class", "inline");
+
+            for (var x = 0; x < 4; x++) {
+                var i = x * 8 + 5;
+                var dayDiv = $("<div>");
+                var dayDate = $("<h4>");
+                var dayIconDiv = $("<img>");
+                dayIcon = response.list[i].weather[0].icon;
+                var dayTemp = $("<p>");
+                var dayHumidity = $("<p>");
+                dayDiv.attr("class", "bg-primary text-white col-md-3 border rounded inline");
+                dayDate.text(response.list[i].dt_txt);
+                dayIconDiv.attr("src", `http://openweathermap.org/img/wn/${dayIcon}@2x.png`);
+                dayTemp.text("Temperature: " + [response.list[i].main.temp] + " °F");
+                dayHumidity.text("Humidity: " + [response.list[i].main.humidity] + "%");
+                dayDiv.append(dayDate, dayIconDiv, dayTemp, dayHumidity);
+                card.append(dayDiv);
+            };
+
+            $("#five-day-forecast").append(card);
         }
     });
 })
+
+
