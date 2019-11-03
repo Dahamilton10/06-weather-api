17da5dbe49973580c887fda5c8968933d6b83a71
David Hamilton <dahamilton10@yahoo.com>
2019-11-03 11:23:01 -0500

    fixed the dynamically created elements not working with the on click function

--- javascript.js
+++ javascript.js
@@ -31,11 +31,14 @@ $("#search-button").click(function () {
         url: `https://api.openweathermap.org/data/2.5/weather?q=${cityQuery},us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial`,
         success: function (response) {
             console.log(response);
+            // creating the input into an li in the history
             var newCard = $("<li>");
-            newCard.data("city", cityQuery);
+            newCard.attr("data-city", cityQuery);
             newCard.text([cityQuery]);
             newCard.addClass("list-group-item");
+            newCard.attr("id", "history");
             $("#search-list").append(newCard);
+            // pulling info from the responce and putting it on the page
             lat = response.coord.lat;
             lon = response.coord.lon;
             console.log(lat, lon);
@@ -50,3 +53,28 @@ $("#search-button").click(function () {
         }
     });
 });
+
+$(document).on("click", "#history", function () {
+    console.log(this);
+    var cityQuery = $(this).data( "city" );
+    console.log(cityQuery);
+    $.ajax({
+        type: "GET",
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${cityQuery},us&appid=064e9a1e286f6e51c5c7c7dd2f6fc716&units=imperial`,
+        success: function (response) {
+            console.log(response);
+            // pulling info from the responce and putting it on the page
+            lat = response.coord.lat;
+            lon = response.coord.lon;
+            console.log(lat, lon);
+            var city = response.name;
+            var temp = response.main.temp;
+            var humidity = response.main.humidity;
+            var wind = response.wind.speed;
+            $("#current-city").text([city]);
+            $("#current-temp").text("Temperature: " + [temp] + " °F");
+            $("#current-humidity").text("Humidity: " + [humidity] + "%");
+            $("#current-wind-speed").text("Wind speed: " + [wind] + " MPH");
+        }
+    });
+})
